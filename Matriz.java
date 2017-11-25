0612a6e582d1caae9a03d699bf09442679d9acc6
Distril8 <33971547+Distril8@users.noreply.github.com>
2017-11-24 21:01:45 -0500

    Add files via upload

---
+++ Matriz.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package matrizdinamica;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author DAVID
+ */
+public class Matriz {
+   public int FILAS, COLUMNAS;
+   public  int[][] A;
+
+    public int getFILAS() {
+        return FILAS;
+    }
+
+    public void setFILAS(int FILAS) {
+        this.FILAS = FILAS;
+    }
+
+    public int getCOLUMNAS() {
+        return COLUMNAS;
+    }
+
+    public void setCOLUMNAS(int COLUMNAS) {
+        this.COLUMNAS = COLUMNAS;
+    }
+
+    public int[][] getA() {
+        return A;
+    }
+
+    public void setA(int[][] A) {
+        this.A = A;
+    }
+   
+
+   
+        
+}
